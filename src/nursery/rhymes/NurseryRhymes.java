acee567446b6be5e254c4c4889b013e8294b83c2
21006 <21006@MININT-VTTS8LK.columbushs.com>
2019-09-19 13:50:23 -0400

    Done

--- src/nursery/rhymes/NurseryRhymes.java
+++ src/nursery/rhymes/NurseryRhymes.java
@@ -24,24 +24,49 @@ public class NurseryRhymes {
         // TODO: make the clock strike three here
         
         // TODO: call your new methods here ( you must write them first! ) 
+        monkeys(10);
+        monkeys(9);
         
+        bruhmode("Medina")
         
     }
-    
+     
     
     
     public static void farm(String animal, String sound){
         // TODO: write your code here
+        System.out.println("Old MacDonald had a farm\n"
+                + "e-i-e-i-o\n"
+                + "And on that farm he had a " + animal + "\n"
+                + "e-i-e-i-o\n"
+                + "With a " + sound + " " + sound + " here\n"
+                + "");
+            
     }
     
     public static void monkeys(int number){
         // TODO: write your code here
+        System.out.println(number + " little monkeys jumping on the bed\n"
+        + "One fell​ ​off​ ​and​ ​bumped​ ​his​ ​head\n"
+        + "Mama called the doctor, and the doctor said\n"
+        + "\"No more monkeys jmping on the bed!\"\n");
     }
     
     public static void hickory_dickory(int time){
         // TODO: write your code here
+        System.out.println("\nHickory dickory dock\n"
+        + "The mouse ran up the clock\n"
+        + "The clock struck\n"
+        + "The mouse ran down\n"
+        + "Hickory dickory dock\n");
     }
     
     // TODO add your new methods here
-    
+    public static void bruhmode(String bruh) {
+        System.out.println(bruh + " can't do a pullup");
+        
+        
+        
+        
+    }
 }
